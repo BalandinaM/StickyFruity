bc3ece42c27722c1b0d3dfeb7b09d14d53ba9681
BalandinaM <marina.balandina.89@mail.ru>
2025-05-09 00:19:28 +0500

    заменила картинку лого на текст

--- sticky/src/header/header.jsx
+++ sticky/src/header/header.jsx
@@ -1,13 +1,19 @@
-import styles from './header.module.scss';
-import logo from './../assets/images/logoStickyFruity.png'
+import styles from "./header.module.scss";
+//import logo from './../assets/images/logoStickyFruity.png'
 
 const Header = () => {
 	return (
-		<header className={styles.header}>
-			<h1 className={styles.visually_hidden}>Онлайн-доска для создания заметок "StickyFruity"</h1>
-			<img className={styles.logo} src={logo} alt="Логотип StickyFruity" width="215" height="45"/>
+		<header className={styles.header} role="banner">
+			<h1 className={styles.logo}>
+				<span className={styles.visually_hidden}>
+					Онлайн-доска для создания заметок "StickyFruity"
+				</span>
+				<span aria-hidden="true">StickyFruity</span>
+			</h1>
+			{/* <img className={styles.logo} src={logo} alt="Логотип StickyFruity" width="215" height="45"/> */}
+			{/* <span >StickyFruity</span> */}
 		</header>
-	)
-}
+	);
+};
 
 export default Header;
