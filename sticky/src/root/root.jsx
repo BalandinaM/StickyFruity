46764f8596d2ae5b533bdba0c65bf64ce32f7213
BalandinaM <marina.balandina.89@mail.ru>
2025-05-29 23:28:10 +0500

    Добавлены стили для кнопки добавления стикеров. И стили при наведении

--- sticky/src/root/root.jsx
+++ sticky/src/root/root.jsx
@@ -59,5 +59,6 @@ const Root = () => {
 
 export default Root;
 
+//текстареа в режиме редактирования не расттянута на весь стикер
 //подумать над стилизацией кнопки добавления стикеров// think from styling button add stickers
 //добавить эффект при наведении на сслыку // added hover-effect to link
